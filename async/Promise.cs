46c415255b9232eee2a51ecb218a0d838ee9c63a
labriola <labriola@digitalprimates.net>
2013-01-08 21:36:34 -0600

    Added a promises/A+ compliant implementation of promises

---
+++ async/Promise.cs
@@ -0,0 +1,204 @@
+﻿/***
+ * Copyright 2012 LTN Consulting, Inc. /dba Digital Primates®
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * @author Michael Labriola <labriola@digitalprimates.net>
+ */
+
+using SharpKit.Html;
+using SharpKit.JavaScript;
+using SharpKit.jQuery;
+
+namespace randori.async {
+
+    [JsType(JsMode.Prototype, Export = false)]
+    public delegate object OnFullfilledDelegate(object result);
+
+    [JsType(JsMode.Prototype, Export = false)]
+    public delegate object OnRejectedDelegate(object reason);
+
+    enum PromiseState {
+        Pending, Rejected, Fullfilled
+    }
+
+    [JsType(JsMode.Json, Export = false, Name = "Object")]
+    class ThenContract {
+        public OnFullfilledDelegate fullfilledHandler;
+        public OnRejectedDelegate rejectedHandler;
+        public Promise promise;
+    }
+
+    public class Promise {
+        readonly JsArray<ThenContract> thenContracts;
+        PromiseState state = PromiseState.Pending;
+
+        public object value;
+        public object reason;
+
+        private bool isFunction( dynamic obj ) {
+            return !!(obj && obj.constructor && obj.call && obj.apply);
+        }
+
+        //3.2.1 Both onFulfilled and onRejected are optional arguments
+        public Promise then(OnFullfilledDelegate onFulfilled=null, OnRejectedDelegate onRejected=null) {
+            var promise = new Promise();
+
+            //3.2.1.1
+            if (!isFunction(onFulfilled)) {
+                onFulfilled = null;
+            }
+
+            //3.2.1.2
+            if (!isFunction(onRejected)) {
+                onRejected = null;
+            }
+
+            //3.2.5
+            var thenContract = new ThenContract{fullfilledHandler = onFulfilled, rejectedHandler = onRejected, promise = promise};
+            thenContracts.push( thenContract );
+
+            if (state == PromiseState.Fullfilled) {
+                //3.2.4
+                HtmlContext.setTimeout(delegate {
+                    fullfill(value);
+                }, 1);
+            } else if (state == PromiseState.Rejected) {
+                //3.2.4
+                HtmlContext.setTimeout(delegate {
+                    internalReject(reason);
+                }, 1);
+            }
+
+            //3.2.6
+            return promise;
+        }
+
+        public void resolve(object response) {
+            //3.2.2 & 3.2.2.3
+            if (state == PromiseState.Pending) {
+
+                //3.1.2.2
+                this.value = response;
+
+                fullfill(response);                    
+            }
+        }
+
+        private void fullfill(object response) {
+
+            //3.1.1.1
+            state = PromiseState.Fullfilled;
+
+            //3.2.2.2
+            while (thenContracts.length > 0) {
+                var thenContract = thenContracts.shift();
+
+                if (thenContract.fullfilledHandler != null) {
+
+                    try {
+                        //3.2.2.1 & 3.2.5.1
+                        dynamic callBackResult = thenContract.fullfilledHandler( response );
+
+                        if ( callBackResult && callBackResult.then != null) {
+                            //3.2.6.3
+                            Promise returnedPromise = callBackResult;
+                            returnedPromise.then(
+                                delegate(object innerResponse) {
+                                    //3.2.6.3.2
+                                    thenContract.promise.resolve(innerResponse);
+                                    return null;
+                                },
+                                delegate(object innerReason) {
+                                    //3.2.6.3.3
+                                    thenContract.promise.reject(innerReason);
+                                    return null;
+                                }
+                             );
+                        } else {
+                            //3.2.6.1
+                            thenContract.promise.resolve( callBackResult );
+                        } 
+                    } catch (JsError error ) {
+                        //3.2.6.2
+                        thenContract.promise.reject( error );
+                    }
+                } else {
+                    //3.2.6.4
+                    thenContract.promise.resolve(response);
+                }
+            }
+        }
+
+        public void reject(object reason) {
+
+            //3.2.3.3
+            if (state == PromiseState.Pending) {
+
+                //3.1.3.2
+                this.reason = reason;
+
+                internalReject(reason);
+            }
+        }
+
+        private void internalReject( object reason ) {
+            //3.1.1.1
+            state = PromiseState.Rejected;
+
+            //3.2.3.2
+            while (thenContracts.length > 0) {
+                var thenContract = thenContracts.shift();
+
+                if (thenContract.rejectedHandler != null) {
+                    try {
+
+                        //3.2.3.1 & 3.2.5.2
+                        dynamic callBackResult = thenContract.rejectedHandler(reason);
+
+                        if (callBackResult && callBackResult.then != null) {
+                            //3.2.6.3
+                            Promise returnedPromise = callBackResult;
+                            returnedPromise.then(
+                                delegate(object innerResponse) {
+                                    //3.2.6.3.2
+                                    thenContract.promise.resolve(innerResponse);
+                                    return null;
+                                },
+                                delegate(object innerReason) {
+                                    //3.2.6.3.3
+                                    thenContract.promise.reject(innerReason);
+                                    return null;
+                                }
+                              );
+                        } else {
+                            //3.2.6.1
+                            thenContract.promise.resolve(callBackResult);
+                        } 
+                    } catch (JsError error) {
+                        //3.2.6.2
+                        thenContract.promise.reject(error);
+                    }
+                } else {
+                    //3.2.6.5
+                    thenContract.promise.reject(reason);
+                }
+            }
+        }
+
+        public Promise() {
+            this.thenContracts = new JsArray<ThenContract>();
+        }
+    }
+}
